68716c99ee58fb2291a1c77d48c1546cf2ad275e
MikeyDN <Mikeydn24@gmail.com>
2022-09-12 22:42:30 +0300

    compiling and saving to file

---
+++ ethereum/compile.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs-extra');
+const solc = require('solc');
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+const contractPath = path.resolve(__dirname, 'contract', 'Campaign.sol');
+const source = fs.readFileSync(contractPath, 'utf8');
+const input = JSON.stringify({
+	language: 'Solidity',
+	sources: { 'Campaign.sol': { content: source } },
+	settings: {
+		outputSelection: { '*': { '*': ['*'] } }
+	}
+});
+const output = JSON.parse(solc.compile(input)).contracts['Campaign.sol'];
+
+fs.ensureDirSync(buildPath);
+
+for (let contract in output) {
+	fs.outputJsonSync(
+		path.resolve(buildPath, contract + '.json'),
+		output[contract]
+	);
+}
