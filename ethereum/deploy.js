d51571afb0ad5d4f94a9104e10771706c1f3321a
MikeyDN <Mikeydn24@gmail.com>
2022-09-14 22:36:59 +0300

    not putting "new" threw error, require compiledfactory.json fixed

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,9 +1,10 @@
 // deploy code will go here
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require("web3");
-const compiledFactory = require("./");
+const compiledFactory = require("./build/CampaignFactory.json");
 
-
+menmonic = 'cousin meadow fix topple rigid news physical occur hockey job stamp step';
+infuraLink = 'https://rinkeby.infura.io/v3/3be66a7d4d014a45a47fdc22233b7b6f';
 
 const provider = new HDWalletProvider(
   menmonic,
@@ -17,9 +18,9 @@ const deploy = async () => {
 
   console.log("Attempting to deploy from account", accounts[0]);
 
-  const result = await new web3.eth.Contract(abi)
-    .deploy({ data: evm.bytecode.object })
-    .send({ gas: '1000000', from: accounts[0] });
+  const result = await new web3.eth.Contract(compiledFactory.abi)
+    .deploy({ data: compiledFactory.evm.bytecode.object })
+    .send({ gas: '1400000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
 
