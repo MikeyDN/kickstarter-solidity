d8b07a06fac0c3429bf859bf09617e19b241824d
MikeyDN <Mikeydn24@gmail.com>
2022-09-14 22:35:58 +0300

    i guess i gave it a bad address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,7 +1,7 @@
 import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 
-const contractAddress = "0x8F899c219803f7bbda964A61aDaf545F8bf49eE2";
+const contractAddress = "0x48EE0A5dD56CC9e17d56377681F9E6826f8e10B8";
 
 const factory = new web3.eth.Contract(CampaignFactory.abi, contractAddress);
 
